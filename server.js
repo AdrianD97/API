67dd6dda2f48efc61910ec69631a9aa205cf4569
AdrianD97 <adrianstefan376@gmail.com>
2020-05-24 19:19:08 +0300

    add missed semicolon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Configuring the database
